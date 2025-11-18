b778330d64ac53f5fc8aa29be6ec80f65a49506d
My6325 <tramy060305@gmail.com>
2025-11-18 22:17:09 +0700

    thiết kế form

---
+++ LapTrinhMang/FrmNhapIP.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace LapTrinhMang
+{
+    partial class FrmNhapIP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIPDau = new System.Windows.Forms.Label();
+            this.lblIPCuoi = new System.Windows.Forms.Label();
+            this.txtIPDau = new System.Windows.Forms.TextBox();
+            this.txtIPCuoi = new System.Windows.Forms.TextBox();
+            this.txtSubnet = new System.Windows.Forms.TextBox();
+            this.lblSubnet = new System.Windows.Forms.Label();
+            this.txtSoMay = new System.Windows.Forms.TextBox();
+            this.lblSoMay = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIPDau
+            // 
+            this.lblIPDau.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblIPDau.AutoSize = true;
+            this.lblIPDau.Location = new System.Drawing.Point(30, 40);
+            this.lblIPDau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIPDau.Name = "lblIPDau";
+            this.lblIPDau.Size = new System.Drawing.Size(60, 20);
+            this.lblIPDau.TabIndex = 0;
+            this.lblIPDau.Text = "IP đầu:";
+            // 
+            // lblIPCuoi
+            // 
+            this.lblIPCuoi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblIPCuoi.AutoSize = true;
+            this.lblIPCuoi.Location = new System.Drawing.Point(30, 91);
+            this.lblIPCuoi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIPCuoi.Name = "lblIPCuoi";
+            this.lblIPCuoi.Size = new System.Drawing.Size(64, 20);
+            this.lblIPCuoi.TabIndex = 1;
+            this.lblIPCuoi.Text = "IP cuối:";
+            // 
+            // txtIPDau
+            // 
+            this.txtIPDau.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIPDau.Location = new System.Drawing.Point(148, 32);
+            this.txtIPDau.Name = "txtIPDau";
+            this.txtIPDau.Size = new System.Drawing.Size(207, 28);
+            this.txtIPDau.TabIndex = 2;
+            // 
+            // txtIPCuoi
+            // 
+            this.txtIPCuoi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIPCuoi.Location = new System.Drawing.Point(148, 83);
+            this.txtIPCuoi.Name = "txtIPCuoi";
+            this.txtIPCuoi.Size = new System.Drawing.Size(207, 28);
+            this.txtIPCuoi.TabIndex = 3;
+            // 
+            // txtSubnet
+            // 
+            this.txtSubnet.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSubnet.Location = new System.Drawing.Point(148, 140);
+            this.txtSubnet.Name = "txtSubnet";
+            this.txtSubnet.Size = new System.Drawing.Size(207, 28);
+            this.txtSubnet.TabIndex = 5;
+            // 
+            // lblSubnet
+            // 
+            this.lblSubnet.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSubnet.AutoSize = true;
+            this.lblSubnet.Location = new System.Drawing.Point(30, 148);
+            this.lblSubnet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSubnet.Name = "lblSubnet";
+            this.lblSubnet.Size = new System.Drawing.Size(103, 20);
+            this.lblSubnet.TabIndex = 4;
+            this.lblSubnet.Text = "SubnetMask:";
+            // 
+            // txtSoMay
+            // 
+            this.txtSoMay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSoMay.Location = new System.Drawing.Point(148, 196);
+            this.txtSoMay.Name = "txtSoMay";
+            this.txtSoMay.Size = new System.Drawing.Size(207, 28);
+            this.txtSoMay.TabIndex = 7;
+            // 
+            // lblSoMay
+            // 
+            this.lblSoMay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSoMay.AutoSize = true;
+            this.lblSoMay.Location = new System.Drawing.Point(30, 204);
+            this.lblSoMay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSoMay.Name = "lblSoMay";
+            this.lblSoMay.Size = new System.Drawing.Size(99, 20);
+            this.lblSoMay.TabIndex = 6;
+            this.lblSoMay.Text = "Số máy con:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(137, 259);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(97, 36);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // FrmNhapIP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 319);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtSoMay);
+            this.Controls.Add(this.lblSoMay);
+            this.Controls.Add(this.txtSubnet);
+            this.Controls.Add(this.lblSubnet);
+            this.Controls.Add(this.txtIPCuoi);
+            this.Controls.Add(this.txtIPDau);
+            this.Controls.Add(this.lblIPCuoi);
+            this.Controls.Add(this.lblIPDau);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmNhapIP";
+            this.Text = "FrmNhapIP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIPDau;
+        private System.Windows.Forms.Label lblIPCuoi;
+        private System.Windows.Forms.TextBox txtIPDau;
+        private System.Windows.Forms.TextBox txtIPCuoi;
+        private System.Windows.Forms.TextBox txtSubnet;
+        private System.Windows.Forms.Label lblSubnet;
+        private System.Windows.Forms.TextBox txtSoMay;
+        private System.Windows.Forms.Label lblSoMay;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
---
+++ LapTrinhMang/ucMayDisconnect.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace LapTrinhMang
+{
+    partial class ucMayDisconnect
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ucMayDisconnect));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtMSSV = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtMSSV
+            // 
+            this.txtMSSV.BackColor = System.Drawing.Color.White;
+            this.txtMSSV.Enabled = false;
+            this.txtMSSV.Location = new System.Drawing.Point(0, 136);
+            this.txtMSSV.Name = "txtMSSV";
+            this.txtMSSV.ShortcutsEnabled = false;
+            this.txtMSSV.Size = new System.Drawing.Size(150, 28);
+            this.txtMSSV.TabIndex = 1;
+            this.txtMSSV.Text = "....";
+            this.txtMSSV.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtIP
+            // 
+            this.txtIP.BackColor = System.Drawing.Color.White;
+            this.txtIP.Enabled = false;
+            this.txtIP.Location = new System.Drawing.Point(0, 169);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.ShortcutsEnabled = false;
+            this.txtIP.Size = new System.Drawing.Size(150, 28);
+            this.txtIP.TabIndex = 2;
+            this.txtIP.Text = "IP: ...";
+            this.txtIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ucMayDisconnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtMSSV);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ucMayDisconnect";
+            this.Size = new System.Drawing.Size(150, 200);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtMSSV;
+        private System.Windows.Forms.TextBox txtIP;
+    }
+}
